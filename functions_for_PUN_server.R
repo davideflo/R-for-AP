a9aa6ec8dec824689305289b4379e5a330bde46b
davideflo <davideflo@users.noreply.github.com>
2016-07-08 17:49:19 +0200

    Add files via upload

---
+++ functions_for_PUN_server.R
@@ -0,0 +1,303 @@
+### functions for PUN
+library(openxlsx)
+library(plyr)
+library(dplyr)
+library(reshape)
+library(stringi)
+library(xlsx)
+library(vioplot)
+library(fda)
+#library(h2o)
+library(TSA)
+library(tseries)
+
+#localH2O <- h2o.init()
+library(TDA)
+library(purrr)
+
+############################################################
+convert_day_to_angle <- function(day)
+{
+  days <- c("dom","lun","mar","mer","gio","ven","sab")
+  ang <- which(days == day)
+  return(cos(ang*pi/7))
+}
+#############################################################
+convert_hour_to_angle <- function(ora)
+{
+  return(sin(ora*pi/24))
+}
+#############################################################
+numeric_days <- function(vec)
+{
+  nd <- rep(0,length(vec))
+  for(i in 1:length(vec))
+  {
+    nd[i] <- convert_day_to_angle(vec[i])
+  }
+  return(nd)
+}
+#############################################################
+subsequent_day <- function(day)
+{
+  if(day == "dom") return("lun")
+  else if (day == "lun") return("mar")
+  else if (day == "mar") return("mer")
+  else if (day == "mer") return("gio")
+  else if (day == "gio") return("ven")
+  else if (day == "ven") return("sab")
+  else return("dom")
+}
+#############################################################
+add_days <- function(first_day, year)
+{
+  dl <- c()
+  if(year %% 4 == 0)
+  {
+    day2 <- subsequent_day(first_day)
+    day3 <- subsequent_day(day2)
+    day4 <- subsequent_day(day3)
+    day5 <- subsequent_day(day4)
+    day6 <- subsequent_day(day5)
+    day7 <- subsequent_day(day6)
+    week <- c(rep(first_day, 24),rep(day2, 24),rep(day3, 24),rep(day4, 24),
+              rep(day5, 24),rep(day6, 24),rep(day7, 24))
+    nw <- trunc(366/7)
+    wr <- 366 - nw*7
+    rdl <- c()
+    for(i in 1:24*wr)
+    {
+      rdl <- c(rdl, week[i])
+    }
+    dl <- c(rep(week,nw),rdl)
+  }
+  else
+  {
+    day2 <- subsequent_day(first_day)
+    day3 <- subsequent_day(day2)
+    day4 <- subsequent_day(day3)
+    day5 <- subsequent_day(day4)
+    day6 <- subsequent_day(day5)
+    day7 <- subsequent_day(day6)
+    week <- c(rep(first_day, 24),rep(day2, 24),rep(day3, 24),rep(day4, 24),
+              rep(day5, 24),rep(day6, 24),rep(day7, 24))
+    nw <- trunc(365/7)
+    wr <- 365 - nw*7
+    rdl <- c()
+    for(i in 1:24*wr)
+    {
+      rdl <- c(rdl, week[i])
+    }
+    dl <- c(rep(week,nw),rdl)
+  }
+  return(dl)
+}
+###################################################################################
+dates <- function(vec)
+{
+  vec <- as.character(vec)
+  dt <- rep(0, length(vec))
+  for(i in 1:length(vec))
+  {
+    dt[i] <- paste0(stri_sub(vec[i],from = 7,to = 8),"/",stri_sub(vec[i],from = 5,to = 6),"/",stri_sub(vec[i],from = 1,to = 4))
+  }
+  return(dt)
+}
+#################################################################################
+add_holidays <- function(vd)
+{
+  ##### codifica numerica delle vacanze
+  ## 1 Gennaio = 1, Epifania = 2
+  ## Pasqua = 3, Pasquetta = 4
+  ## 25 Aprile = 5, 1 Maggio = 6, 2 Giugno = 7,
+  ## Ferragosto = 8, 1 Novembre = 9
+  ## 8 Dicembre = 10, Natale = 11, S.Stefano = 12, S.Silvestro = 13
+  holidays <- rep(0,length(vd))
+  pasqua <- c("04/04/2010", "24/04/2011", "08/04/2012", "31/03/2013", "20/04/2014", "05/04/2015", "27/03/2016")
+  pasquetta <- c("05/04/2010", "25/04/2011", "09/04/2012", "01/04/2013", "21/04/2014", "06/04/2015", "28/03/2016")
+  for(i in 1:length(vd))
+  {
+    if(stri_sub(vd[i],from=1,to=5) == "01/01") holidays[i] <- 1
+    else if(stri_sub(vd[i],from=1,to=5) == "06/01") holidays[i] <- 2
+    else if(stri_sub(vd[i],from=1,to=5) == "25/04") holidays[i] <- 5
+    else if(stri_sub(vd[i],from=1,to=5) == "01/05") holidays[i] <- 6
+    else if(stri_sub(vd[i],from=1,to=5) == "02/06") holidays[i] <- 7
+    else if(stri_sub(vd[i],from=1,to=5) == "15/08") holidays[i] <- 8
+    else if(stri_sub(vd[i],from=1,to=5) == "01/11") holidays[i] <- 9
+    else if(stri_sub(vd[i],from=1,to=5) == "08/12") holidays[i] <- 10
+    else if(stri_sub(vd[i],from=1,to=5) == "25/12") holidays[i] <- 11
+    else if(stri_sub(vd[i],from=1,to=5) == "26/12") holidays[i] <- 12
+    else if(stri_sub(vd[i],from=1,to=5) == "31/12") holidays[i] <- 13
+    else if(vd[i] %in% pasqua) holidays[i] <- 3
+    else if(vd[i] %in% pasquetta) holidays[i] <- 4
+  }
+  return(holidays)
+}
+#########################################################################################
+data_successiva <- function(date)
+{
+  d <- strsplit(date, "/")
+  
+  day <- as.numeric(d[1])
+  month <- as.numeric(d[2])
+  year <- as.numeric(d[3])
+  
+  leap_year <- year %% 4
+  
+  new_day <- 0
+  new_month <- month
+  new_year <- year
+  
+  df31 <- c(1,2,4,6,8,9,11)
+  df30 <- c(5,7,10,12)
+  
+  if(month %in% df31 & day == 31) {new_day <- 1; new_month <- month + 1}
+  else if(month %in% df30 & day == 30) {new_day <- 1; new_month <- month + 1}
+  else if(month == 2 & leap_year != 0 & day == 28) {new_day <- 1; new_month <- month + 1}
+  else if(month == 2 & leap_year == 0 & day == 29) {new_day <- 1; new_month <- month + 1}
+  else new_day <- day + 1
+  
+  if(new_month == 13) new_month <- 1
+  
+  if(month == 12 & day == 31) new_year <- year + 1
+  
+  if(new_month < 10) new_month <- paste0("0", new_month)
+  
+  if(new_day < 10) new_day <- paste0("0", new_day)
+  
+  new_date <- paste0(new_day, "/", new_month, "/", new_year)
+  
+  return(new_date)
+}
+#########################################################################################
+associate_meteo_ora <- function(data, meteo, meteovar)
+{
+  vm <- rep(0, length(data))
+  for(d in 1:length(data))
+  {
+    ir <- which(meteo["Data"] == data[d])
+    vm[d] <- as.numeric(meteo[ir,meteovar])
+    
+  }
+  return(vm)
+}
+#########################################################################################
+trova_date_mancanti <- function(date, meteo)
+{
+  dm <- c()
+  for(d in date)
+  {
+    if(length( which(meteo[,1] == d) ) == 0 ) dm <- c(dm,d)
+  }
+  return(dm)
+}
+#########################################################################################
+associate_days <- function(ora, day)
+{
+  vdays <- rep(day, 24)
+  for(i in 1:(length(ora)-1) )
+  {
+    if(ora[i] == 24 & ora[i+1] == 1) 
+    {
+      index <- i+1
+      for(j in index:24) vdays[j] <- subsequent_day(day)
+    }
+  }
+  return(vdays)
+}
+########################################################################################
+create_dataset <- function(pun, first_day)
+{
+  d_f <- data_frame()
+  Names <- c(paste0("pun-",24:1), paste0("aust-",24:1), paste0("cors-",24:1), paste0("fran-",24:1), paste0("grec-",24:1),
+             paste0("slov-",24:1), paste0("sviz-",24:1), paste0("angleday-",24:1), paste0("holiday-",24:1), "y",paste0("day-",24:1))
+  for(i in 1:(nrow(pun)-23))
+  {
+    #print(i)
+    y <- p <- aus <- cors <- fran <- grec <- slov <- sviz <- ora <- dat <- c()
+    for(j in i:(i+23))
+    {
+      p <- c(p, pun[j,"PUN"]); aus <- c(aus, pun[j,"AUST"]); cors <- c(cors, pun[j,"CORS"])
+      fran <- c(fran, pun[j,"FRAN"]); grec <- c(grec, pun[j,"GREC"]); slov <- c(slov, pun[j,"SLOV"])
+      sviz <- c(sviz, pun[j,"SVIZ"]); ora <- c(ora, pun[j,"Ora"]); dat <- c(dat, pun[j,"Data/Date"]) 
+    }
+    y <- c(y, pun[(i+24),"PUN"])
+    day <- unlist(ifelse(nrow(d_f) > 0, d_f[nrow(d_f),ncol(d_f)], first_day))
+    #print(day)
+    ds <- dates(dat)
+    hol <- add_holidays(ds)
+    vdays <- associate_days(ora, day)
+    vdays2 <- maply(1:24, function(n) as.character(vdays[n]))
+    aday <- maply(1:24, function(n) convert_day_to_angle(vdays2[n]))
+    
+    adf <- data.frame(t(p), t(aus), t(cors), t(fran), t(grec), t(slov), t(sviz), t(aday), t(hol), y, t(vdays), stringsAsFactors = FALSE)
+    colnames(adf) <- Names
+    
+    d_f <- bind_rows(d_f, adf)
+#    d_f %>% map_if(is.factor, as.character) -> d_f
+  }
+  colnames(d_f) <- Names
+  return(d_f)
+}
+######################################################
+create_dataset23 <- function(pun, first_day, varn, meteo)
+{
+  d_f <- data_frame()
+  Names <- c(paste0(varn,"-",23:1), paste0("aust-",23:1), paste0("cors-",23:1), paste0("fran-",23:1), paste0("grec-",23:1),
+             paste0("slov-",23:1), paste0("sviz-",23:1), paste0("angleday-",23:1), paste0("holiday-",23:1), "y",
+             paste0("angleora-",23:1),
+             paste0("tmin-",23:1), paste0("tmax-",23:1), paste0("tmed-",23:1), paste0("rain-",23:1), paste0("vento-",23:1), 
+             paste0("day-",23:1))
+  for(i in 1:(nrow(pun)-23))
+  {
+    #print(i)
+    y <- p <- aus <- cors <- fran <- grec <- slov <- sviz <- ora <- dat <- c()
+    for(j in i:(i+22))
+    {
+      p <- c(p, pun[j,varn]); aus <- c(aus, pun[j,"AUST"]); cors <- c(cors, pun[j,"CORS"])
+      fran <- c(fran, pun[j,"FRAN"]); grec <- c(grec, pun[j,"GREC"]); slov <- c(slov, pun[j,"SLOV"])
+      sviz <- c(sviz, pun[j,"SVIZ"]); ora <- c(ora, pun[j,2]); dat <- c(dat, pun[j,1]) 
+    }
+    y <- c(y, pun[(i+23),varn])
+    day <- unlist(ifelse(nrow(d_f) > 0, d_f[nrow(d_f),ncol(d_f)], first_day))
+    #print(day)
+    ds <- dates(dat)
+    hol <- add_holidays(ds)
+    vdays <- associate_days(ora, day)
+    vdays2 <- maply(1:24, function(n) as.character(vdays[n]))
+    aday <- maply(1:24, function(n) convert_day_to_angle(vdays2[n]))
+    ahour <- convert_hour_to_angle(ora)
+    ## togli vdays e metti variabili meteo
+    tmin <- associate_meteo_ora(ds, meteo, "Tmin")
+    tmax <- associate_meteo_ora(ds, meteo, "Tmax")
+    tmed <- associate_meteo_ora(ds, meteo, "Tmedia")
+    rain <- associate_meteo_ora(ds, meteo, "Pioggia")
+    vm <- associate_meteo_ora(ds, meteo, "Vento_media")
+    ### transpose the vectors as they are column vectors in R
+    adf <- data.frame(t(p), t(aus), t(cors), t(fran), t(grec), t(slov), t(sviz), t(aday[1:23]), t(hol[1:23]), y, t(ahour[1:23]),
+                      t(tmin[1:23]), t(tmax[1:23]), t(tmed[1:23]), t(rain[1:23]), t(vm[1:23]), t(vdays[1:23]), stringsAsFactors = FALSE)
+    colnames(adf) <- Names
+    
+    d_f <- bind_rows(d_f, adf)
+  }
+  colnames(d_f) <- Names
+  return(d_f[,1:346])
+}
+######################################################
+sign_process <- function(Pt)
+{
+  St <- rep(0, (length(Pt)-1))
+  for(i in 1:length(St)) {print(i);St[i] <- sign(Pt[i+1] - Pt[i])}
+  
+  return(St)
+}
+######################################################
+percentage_greater_than <- function(x, p)
+{
+  return(length(x[x > p])/length(x))
+}
+#####################################################
+RMSE <- function(x)
+{
+  return(sqrt(mean(x^2)))
+}
---
+++ pun_server.R
@@ -0,0 +1,461 @@
+########## forecast PUN orario ###
+library(openxlsx)
+library(dplyr)
+library(reshape)
+library(stringi)
+library(xlsx)
+library(vioplot)
+library(fda)
+library(h2o)
+library(TSA)
+library(tseries)
+
+library(TDA)
+library(forecast)
+library(rugarch)
+
+library(ggplot2)
+source("R_code/functions_for_PUN_server.R")
+
+prices10 <- openxlsx::read.xlsx("C:/Users/utente/Documents/PUN/Anno 2010.xlsx", sheet="Prezzi-Prices", colNames=TRUE)
+prices11 <- openxlsx::read.xlsx("C:/Users/utente/Documents/PUN/Anno 2011.xlsx", sheet="Prezzi-Prices", colNames=TRUE)
+prices12 <- openxlsx::read.xlsx("C:/Users/utente/Documents/PUN/Anno 2012.xlsx", sheet="Prezzi-Prices", colNames=TRUE)
+prices13 <- openxlsx::read.xlsx("C:/Users/utente/Documents/PUN/Anno 2013.xlsx", sheet="Prezzi-Prices", colNames=TRUE)
+prices14 <- openxlsx::read.xlsx("C:/Users/utente/Documents/PUN/Anno 2014.xlsx", sheet="Prezzi-Prices", colNames=TRUE)
+prices15 <- openxlsx::read.xlsx("C:/Users/utente/Documents/PUN/Anno 2015.xlsx", sheet="Prezzi-Prices", colNames=TRUE)
+prices16 <- openxlsx::read.xlsx("C:/Users/utente/Documents/PUN/Anno 2016_04.xlsx", sheet="Prezzi-Prices", colNames=TRUE)
+
+meteonord <- read.csv2("C:/Users/utente/Documents/PUN/storico_milano.txt", header=TRUE, sep="\t",colClasses = "character", stringsAsFactors = FALSE)
+meteocsud <- read.csv2("C:/Users/utente/Documents/PUN/storico_roma.txt", header=TRUE, sep="\t",colClasses = "character", stringsAsFactors = FALSE)
+
+
+
+### https://cran.r-project.org/web/views/TimeSeries.html
+plot(1:length(unlist(prices10["PUN"])), unlist(prices10["PUN"]), type = "l", lwd=2, col="blue")
+acf(unlist(prices10["CSUD"]), lag.max = 100)
+acf(as.numeric(unlist(meteocsud["Tmedia"])), lag.max = 365)
+#plot(stl(ts(unlist(prices10["PUN"]),frequency=365),s.window=7))
+plot(se <- stl(ts(unlist(prices10["CSUD"]),frequency=24),s.window="periodic")) ## questo mi pare piu corretto dalla descrizione dell'help in R
+plot(stl(ts(unlist(prices10["PUN"]),frequency=24),s.window="periodic"))
+plot(stl(ts(as.numeric(unlist(meteocsud["Tmedia"])),frequency=365),s.window="periodic"))
+#plot(stl(ts(unlist(prices10["PUN"]),frequency=12),s.window=7))
+
+p10 <- prices10[,3:21]
+cor(p10)
+
+dts <- unique(dates(prices15[,1]))
+tdm <- trova_date_mancanti(dts, meteo = meteocsud)
+tdm
+
+test <- create_dataset(prices10, "ven")
+test <- test[1:8736,]
+test23 <- create_dataset23(prices10, "ven", "CSUD", meteocsud)
+csud11 <- create_dataset23(prices11, "sab", "CSUD", meteocsud)
+
+
+library(h2o)
+##### NOTA BENE: se h2o non parte, vai in C:\Users\d_floriello\Documents\R\R-3.3.1\library\h2o\java e fai partire il JAR file!!!!!
+## http://localhost:54321/flow/index.html ## flow
+h2o.init(nthreads = -1)
+
+train <- as.h2o(test23[1:7000,])
+val <- as.h2o(test23[7001:8737,])
+dl <- h2o.deeplearning(names(train), "y", training_frame = train, validation_frame = val, activation = "Tanh",
+                       hidden = c(365, 52, 12, 4), epochs = 100)
+pred <- h2o.predict(dl, val)
+
+plot(dl) 
+
+a <- as.numeric(pred$predict) ### <- estrae i valori da pred (H2OFrame Class) 
+a <- as.matrix(a)
+
+plot(a, type="l",col="blue")
+lines(unlist(test23[,208]), type="o",col="red")
+
+yy <- unlist(test23[7001:8737,"y"])
+diff <- yy - a
+mean(diff)
+sd(diff)
+
+hist(diff,freq = FALSE)
+lines(density(diff))
+
+apdiff <- abs(diff)/unlist(test23[7001:8737,"y"])
+
+for(p in c(1:10)/10) print(percentage_greater_than(apdiff,p))
+
+std_diff <- (diff - mean(diff))/sd(diff)
+
+hist(std_diff,freq = FALSE)
+lines(density(std_diff))
+
+shapiro.test(std_diff)
+qqnorm(diff)
+lines(seq(-3,3,0.001),seq(-3,3,0.001),type="l",col="red")
+
+cor(yy,diff) ### <- explained by regression to the mean
+cor(yy,apdiff)
+par(mfrow = c(2,1))
+plot(yy, type="o",col="red")
+plot(diff, type="o")
+########### distribution free qqplot ################################
+qq <- qt(p = seq(0, 1, length.out = length(diff)), df = length(diff)-1 )
+plot(qq,sort(diff,decreasing=FALSE))
+lines(seq(-3,3,0.001),seq(-3,3,0.001),type="l",col="red")
+#####################################################################
+
+pred_tot <- predict(dl, as.h2o(test23))
+
+cts <- as.numeric(pred_tot$predict) ### <- estrae i valori da pred (H2OFrame Class) 
+cts <- unlist(as.matrix(cts)[,1])
+plot(dlts <- stl(ts(cts,frequency=24),s.window="periodic"))
+
+dlts$time.series
+min_season <- dlts$time.series[1:24,1]
+min_season_orig <- se$time.series[1:24,1]
+par(mfrow = c(2,1))
+plot(min_season, type="l", col="blue")
+plot(min_season_orig, type= "o", col="red")
+
+dl.trend <- unlist(dlts$time.series[,2])
+se.trend <- unlist(se$time.series[,2])
+
+plot(dl.trend, type="l", col="blue")
+plot(se.trend, type= "l", col="red")
+
+sqrt(mean((dl.trend - se.trend)^2))
+RMSE(cts - test23$y)
+#############################################################################
+###### test: 2010 on 2011 ###################################################
+test23 <- create_dataset23(prices10, "ven", "CSUD", meteocsud)
+csud11 <- create_dataset23(prices11, "sab", "CSUD", meteocsud)
+
+train10 <- as.h2o(test23)
+val11 <- as.h2o(csud11)
+
+start.time <- Sys.time()
+dl11 <- h2o.deeplearning(names(train10), "y", training_frame = train10, validation_frame = val11, activation = "Tanh",
+                       hidden = c(8760,365,52,12,4), epochs = 100)
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+plot(dl11)
+
+pred11 <- predict(dl11, val11)
+
+p11 <- as.numeric(pred11$predict) 
+p11 <- as.matrix(p11)
+
+plot(p11, type="l",col="blue", xlab="time", ylab="euro/MWh", main="CSUD11 calcolato vs vero")
+lines(unlist(csud11$y), type="o",col="red")
+
+yy11 <- unlist(csud11$y)
+diff11 <- yy11 - unlist(p11)
+mean(diff11)
+sd(diff11)
+median(diff11)
+
+plot(density(diff11),main="distribuzione degli errori")
+hist(diff11,freq = FALSE, add=TRUE)
+
+apdiff11 <- abs(diff11)/yy11
+
+for(p in c(1:10)/10) print(percentage_greater_than(apdiff11,p))
+
+std_diff11 <- (diff11 - mean(diff11))/sd(diff11)
+
+hist(std_diff11,freq = FALSE)
+lines(density(std_diff11))
+
+shapiro.test(std_diff11)
+qqnorm(diff11)
+lines(seq(-20,20,0.0001),seq(-20,20,0.0001),type="l",col="red")
+
+cor(yy11,diff11) 
+cor(yy11,apdiff11) ## <- almost independent
+
+plot(dlts11 <- stl(ts(unlist(p11[,1]),frequency=24),s.window="periodic"),main="serie stimata")
+plot(se11 <- stl(ts(unlist(csud11$y),frequency=24),s.window="periodic"),main="serie vera")
+#dlts11$time.series
+
+min_season11 <- dlts11$time.series[1:24,1]
+min_season_orig11 <- se11$time.series[1:24,1]
+par(mfrow = c(2,1))
+plot(min_season11, type="l", col="blue")
+plot(min_season_orig11, type= "o", col="red")
+
+dl11.trend <- unlist(dlts11$time.series[,2])
+se11.trend <- unlist(se11$time.series[,2])
+
+plot(dl11.trend, type="l", col="blue")
+plot(se11.trend, type= "l", col="red")
+
+RMSE(dl11.trend - se11.trend)
+RMSE(unlist(p11[,1]) - csud11$y)
+
+par(mfrow=c(1,1))
+plot(ud11 <- unlist(diff11[,1]), type="l",xlab="time",ylab="euro/MWh", main = "error")
+plot(pud11 <- unlist(apdiff11[,1]), type="l",xlab="time",ylab="%", main = "errore percentuale")
+############################################################################
+################# test: csud 2010 - 2014 on 2015 ###########################
+############################################################################
+variables <- colnames(prices10)[c(1:12,14:21)]
+prices <- rbind(prices10[c(1:12,14:21)], prices11[,which(colnames(prices11) %in% variables)], prices12[,which(colnames(prices12) %in% variables)], 
+                prices13[,which(colnames(prices13) %in% variables)], prices14[,which(colnames(prices14) %in% variables)])
+
+test04 <- create_dataset23(prices, "ven", "CSUD",meteocsud)
+val15 <- create_dataset23(prices15, "gio", "CSUD",meteocsud)
+
+train_tot <- as.h2o(test04)
+val <- as.h2o(val15)
+
+dltot <- h2o.deeplearning(names(train_tot), "y", training_frame = train_tot, validation_frame = val, activation = "Tanh",
+                         hidden = c(365, 52, 12, 4), epochs = 100)
+plot(dltot)
+
+predtot <- predict(dltot, val)
+
+pt <- as.numeric(predtot$predict) 
+pt <- as.matrix(pt)
+
+plot(pt, type="l",col="blue", xlab="time", ylab="euro/MWh", main="CSUD15 calcolato vs vero")
+lines(unlist(val15$y), type="o",col="red")
+
+yy15 <- unlist(val15$y)
+diff <- yy15 - unlist(pt)
+mean(diff)
+sd(diff)
+median(diff)
+
+plot(density(diff),main="distribuzione degli errori")
+hist(diff,freq = FALSE, add=TRUE)
+
+apdiff <- abs(diff)/yy15
+
+for(p in c(1:10)/10) print(percentage_greater_than(apdiff,p))
+
+std_diff <- (diff - mean(diff))/sd(diff)
+
+#shapiro.test(std_diff)
+qqnorm(diff)
+lines(seq(-20,20,0.0001),seq(-20,20,0.0001),type="l",col="red")
+
+cor(yy15,diff) 
+cor(yy15,apdiff) ## <- almost independent
+
+plot(dlts <- stl(ts(unlist(pt[,1]),frequency=24),s.window="periodic"),col="blue",main="serie stimata")
+plot(se15 <- stl(ts(unlist(val15$y),frequency=24),s.window="periodic"),col="red",main="serie vera")
+#dlts11$time.series
+
+min_season <- dlts$time.series[1:24,1]
+min_season_orig15 <- se15$time.series[1:24,1]
+par(mfrow = c(2,1))
+plot(min_season, type="l", col="blue")
+plot(min_season_orig15, type= "o", col="red")
+
+dl.trend <- unlist(dlts$time.series[,2])
+se15.trend <- unlist(se15$time.series[,2])
+
+plot(dl.trend, type="l", col="blue")
+plot(se15.trend, type= "l", col="red")
+
+RMSE(dl.trend - se15.trend)
+RMSE(unlist(pt[,1]) - val15$y)
+
+##############################################################################
+
+plot(1:1736,diff[1:1736,1],type="p",lwd=2,col="red")
+abline(h =  colMeans(diff), lwd=2, col="black")
+
+sp_test <- sign_process(unlist(test[7001:8736,"y"]))
+at <- sign_process(a)
+
+plot(sp_test, type= "o")
+
+pt <- sp_test * at
+
+length(which(pt <= 0))/length(pt)
+
+#### test with forecast
+fit_ts <- arfima(test$y)
+
+Yt <- sign_process(test$y)
+
+fr <- forecast(fit_ts, h = 24)
+
+testy <- test[,c(1:216)]
+
+fit_ari <- auto.arima(test$y, xreg = testy)
+summary(fit_ari)
+
+## test with autoregressive neural networks
+
+f <- data.frame(coef = fft(unlist(test["y"])), freqindex = c(1:nrow(test)))
+mc <- Mod(f$coef)[2:length(f$freqindex)]
+qplot(freqindex, Mod(coef), data = f[3000:5000,] ,geom = "line")
+
+f[Mod(f$coef) > 1500 & f$freqindex < 5000, "freqindex"] - 1
+
+peaks <- Mod(f$coef) > 1500
+ffilt <- f
+ffilt[!peaks, "coef"] <- 0
+ffilt <- data.frame(index=ffilt$freqindex[1:5000], value=Re(fft(ffilt$coef[1:5000], inverse=TRUE))/5000, type=rep("filtered", times=5000))
+ffilt2 <- data.frame(index=1:5000, value=test[1:5000,"y"], type=rep("original", times=5000))
+names(ffilt2) <- names(ffilt)
+ffilt <- rbind(ffilt, ffilt2)
+
+qplot(index, value, data = ffilt, colour = as.factor(type), geom = "line")
+
+midindex <- ceiling((length(f$coef)-1)/ 2) + 1
+peakind <- f[abs(f$coef) > 1000 & f$freqindex > 1 & f$freqindex < midindex,]
+lindex <- length(f$coef)
+
+lowerind <- 1
+
+subsignals <- lapply(c(peakind$freqindex, midindex+1), function(x){
+  upperind <- x
+  fsub <- f
+  notnullind <- ((fsub$freqindex >= lowerind
+                  & fsub$freqindex < upperind)
+                 |
+                   (fsub$freqindex >  (lindex - upperind + 2)
+                    & fsub$freqindex <= (lindex - lowerind + 2)))
+  fsub[!notnullind,"coef"] <- 0
+  lowerind <<- upperind
+  Re(fft(fsub$coef, inverse=TRUE)/length(fsub$coef))
+})
+
+library(grid)
+grid.newpage()
+pushViewport(viewport(layout=grid.layout(4,2)))
+
+vplayout <- function(x,y)
+  viewport(layout.pos.row = x, layout.pos.col = y)
+
+psig <- function(x, y, z){
+  h <- data.frame(index = c(1:length(subsignals[[x]])),
+                  value = subsignals[[x]])
+  lab <- paste("Subseries ", as.character(x), sep="")
+  print(qplot(index, value, data = h, geom = "line", main=lab), vp = vplayout(y,z))
+  TRUE
+}
+
+psig(1,1,1); psig(2,1,2); psig(3,2,1); psig(4,2,2); psig(5,3,1); psig(6,3,2); psig(7,4,1); psig(8,4,2)
+
+library(nnet)
+nn.sizes <- c(2,2,3,4,5,6,7,8)
+
+numofsubs <- length(subsignals)
+twindow <- 4
+
+offsettedsubdfs <- lapply(1:numofsubs, function(x){
+  singleoffsets <- lapply(0:(twindow-1), function(y){
+    subsignals[[x]][(twindow-y):(length(subsignals[[x]])-y-1)]
+  })
+  a <- Reduce(cbind, singleoffsets)
+  names <- lapply(1:twindow, function(y){paste("TS", as.character(x), "_", as.character(y), sep = "")})
+  b <- as.data.frame(a)
+  colnames(b) <- names
+  b
+})
+
+sample.number <- length(offsettedsubdfs[[1]][,1])
+
+nns <- lapply(1:length(offsettedsubdfs), function(i)
+  
+{
+  
+  nn <- nnet(offsettedsubdfs[[i]][1:(sample.number),], #the training samples
+             
+             subsignals[[i]][(twindow+1):(length(subsignals[[i]]))], #the output
+             
+             #corresponding to the training samples
+             
+             size=nn.sizes[i], #number of neurons
+             
+             maxit = 1000, #number of maximum iteration
+             
+             linout = TRUE) #the neuron in the output layer should be linear
+  
+  #the result of the trained networks should be plotted
+  
+  plot(subsignals[[i]][(twindow+1):(length(subsignals[[i]]))], type="l")
+  
+  lines(nn$fitted.values,type="l",col="red")
+  
+  nn
+  
+})
+
+number.of.predict <- 14
+
+#long term prediction
+
+long.predictions <- lapply(1:length(offsettedsubdfs), function(i)
+  
+{
+  
+  prediction <- vector(length=number.of.predict, mode="numeric")
+  
+  #initial input
+  
+  input <- offsettedsubdfs[[i]][sample.number,]
+  
+  for (j in 1 : number.of.predict)
+    
+  {
+    
+    prediction[j] <- predict(nns[[i]], input)
+    
+    input <- c(prediction[j],input[1:(length(input)-1)])
+    
+  }
+  
+  #we want to plot the prediction
+  
+  plot(c(nns[[i]]$fitted.values,prediction), type="l",col="red")
+  
+  lines(subsignals[[i]][(twindow+1):length(subsignals[[i]])])
+  
+  prediction
+  
+})
+
+gc()
+nn <- nnet(x= test[,1:216], y = test[,217], size=c(52,24,12,4),data=test,entropy=TRUE, maxit = 1000, MaxNWts = 100000,linout = FALSE)
+~
+test2 <- test[,1:217]
+nn <- nnet(test2$y ~ ., data=test2, size=100, rang=0.5, maxit = 1000, MaxNWts = 100000,linout = TRUE)
+
+  
+library(tsDyn)
+modar <- aar(test$y, m=1,d=24,steps=24)
+plot(modar)
+
+plot(modar$fitted.values, col="blue", type="l", lwd=2)
+lines(test$y, col="red", type="l", lwd=1.5)
+
+diff2 <- test$y - modar$fitted.values
+
+library(forecast)
+
+nnf <- nnetar(test2$y, P = 24, size = 1, xreg = test2[,1:216], h = 1)
+
+library(arfima)
+arf <- arfima::arfima(prices10$PUN, xreg= prices10[,c(4,8,11,12,19,21)])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
