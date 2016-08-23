5a1f75a8aa67b77b5ebf94f66302dc24034f3195
davideflo <davideflo@users.noreply.github.com>
2016-08-23 17:59:45 +0200

    enhanced fixed models

--- DL_fixed.R
+++ DL_fixed.R
@@ -24,25 +24,30 @@ for(da in 1:5)
         name1 <- paste0("C:\\Users\\utente\\Documents\\PUN\\fixed\\trainset_step_",step,"_dayahead_",da,".csv")
         name2 <- paste0("C:\\Users\\utente\\Documents\\PUN\\fixed\\testset_step_",step,"_dayahead_",da,".csv")  
         
-        train <- read.csv2(name1, header = TRUE, sep = ",", colClasses = "character", stringsAsFactors = FALSE)
-        test <- read.csv2(name2, header = TRUE, sep = ",", colClasses = "character", stringsAsFactors = FALSE)
+        train <- data.frame(read.csv2(name1, header = TRUE, sep = ",", colClasses = "character", stringsAsFactors = FALSE))
+        test <- data.frame(read.csv2(name2, header = TRUE, sep = ",", colClasses = "character", stringsAsFactors = FALSE))
         
-        trainseth2o <- as.h2o(data.matrix(train))
-        testseth2o <- as.h2o(data.matrix(test))
+        Y <- as.numeric(unlist(test["y"]))
+        
+        train <- data.matrix(train)
+        test <- data.matrix(test)
+        
+        trainseth2o <- as.h2o(train)
+        testseth2o <- as.h2o(test)
         
         #drop <- names(trainseth2o)[grep("angleora",names(trainseth2o))]
         #predictors <- setdiff(names(trainseth2o), union(response,drop))
         
         predictors <- setdiff(names(trainseth2o), response)
         
-        while(r2 <= 0)
+        while(r2 <= 0 | is.na(r2) | is.nan(r2) )
         {
           
           model <- h2o.deeplearning(x = predictors, y = response, training_frame = trainseth2o, model_id = id, validation_frame = testseth2o, standardize = TRUE,
                                     activation = "Rectifier", hidden = c(2188,365,52,12,6), epochs = 100, max_w2 = 100, l1=1e-5)
           
           r2 <- h2o.r2(model, train = FALSE, valid = TRUE)
-        
+          print(r2)
         }
         
         
@@ -50,13 +55,8 @@ for(da in 1:5)
 
         df <- data.frame(as.character(id), t(h2o.r2(model, train = TRUE, valid = TRUE)), t(h2o.mse(model, train = TRUE, valid = TRUE)))
         res <- bind_rows(res, df)
-                
-        h2o.rm(trainseth2o); h2o.rm(testseth2o);
-        h2o.rm(model)
-        print(paste("done step",step,"day ahead", da, "and removed the files"))
         
         ### ERROR DISTRIBUTION FOR BOOTSTRAP ##########
-        Y <- as.numeric(unlist(test$y))
         pred <- predict(model, testseth2o)
         
         pt <- as.numeric(pred$predict) 
@@ -67,6 +67,10 @@ for(da in 1:5)
         
         xlsx::write.xlsx(data.frame(diff),paste0("C:\\Users\\utente\\Documents\\PUN\\fixed\\errors\\distribution_errors_step_",step,"_dayahead_",da,".xlsx"))
         ###############################################
+                
+        h2o.rm(trainseth2o); h2o.rm(testseth2o);
+        h2o.rm(model)
+        print(paste("done step",step,"day ahead", da, "and removed the files"))
         
         end <- Sys.time()
         print(end-start)
@@ -78,6 +82,7 @@ for(da in 1:5)
       {
         
         message(cond)
+        traceback()
         missed <- c(missed, id)
         print(paste("day ahead", da, "and step", step, "failed"))
         
@@ -88,7 +93,6 @@ for(da in 1:5)
 
 xlsx::write.xlsx(res,"performance_DLFixed_models.xlsx", row.names = TRUE, col.names = TRUE)
 write.table(as.data.frame(missed), "missed_models.txt")
-
 
 
 
