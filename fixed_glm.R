533f04b4c789cf31ca6acad1b6291fde7ed94c08
davideflo <davideflo@users.noreply.github.com>
2016-09-07 15:04:38 +0200

    complete code for glm dataset

---
+++ fixed_glm.R
@@ -0,0 +1,32 @@
+###### generate all glm datasets #######
+
+library(readxl)
+library(plyr)
+library(dplyr)
+
+source("C://Users//utente//Documents//R_code//functions_for_PPIA_server.R")
+source("C://Users//utente//Documents//R_code//functions_for_PUN_server.R")
+source("C://Users//utente//Documents//glm_dataset.R")
+
+
+prices16 <- read_excel("C:/Users/utente/Documents/PUN/Anno 2016_08.xlsx", sheet="Prezzi-Prices")
+
+mi6 <- read_excel("C:/Users/utente/Documents/PUN/Milano 2016.xlsx", sheet= 1)
+ro6 <- read_excel("C:/Users/utente/Documents/PUN/Roma 2016.xlsx", sheet= 1)
+fi6 <- read_excel("C:/Users/utente/Documents/PUN/Firenze 2016.xlsx", sheet= 1)
+pa6 <- read_excel("C:/Users/utente/Documents/PUN/Palermo 2016.xlsx", sheet= 1)
+ca6 <- read_excel("C:/Users/utente/Documents/PUN/Cagliari 2016.xlsx", sheet= 1)
+rc6 <- read_excel("C:/Users/utente/Documents/PUN/Reggio Calabria 2016.xlsx", sheet= 1)
+
+mi6 <- get_meteo(mi6)
+ro6 <- get_meteo(ro6)
+fi6 <- get_meteo(fi6)
+pa6 <- get_meteo(pa6)
+ca6 <- get_meteo(ca6)
+rc6 <- get_meteo(rc6)
+
+meteoav16 <- mediate_meteos(mi6, ro6, fi6, pa6, ca6, rc6, FALSE)
+
+library(h2o)
+
+generate_glm_datasets(prices16, meteoav16)
--- glm_dataset.R
+++ glm_dataset.R
@@ -24,7 +24,7 @@ dates2 <- function(vd)
 ###############################################################################################################################################
 create_glm_dataset <- function(pun, varn, regressorh, targeth, meteo, day_ahead)
 {
-  Names <- c(varn,"aust","cors","fran","grec",
+  vanames <- c(varn,"aust","cors","fran","grec",
              "slov","sviz","angleday","holiday",
              "tmin","tmax", "tmed", "rain", "vento",
              "y","target_day", "target_holiday","target_tmin","target_tmax","target_tmed","target_pioggia","target_vento")
@@ -35,38 +35,45 @@ create_glm_dataset <- function(pun, varn, regressorh, targeth, meteo, day_ahead)
     return(0)
   }
   
-  variables <- which(tolower(colnames(pun)) %in% c(varn,"aust","cors","fran","grec","slov","sviz"))
+  variables <- which(tolower(colnames(pun)) %in% c("pun","aust","cors","fran","grec","slov","sviz"))
   
   regh <- which(pun[,2] == regressorh)
   tarh <- which(pun[,2] == targeth)
   start_ix <- 1
   
   reg_val <- pun[regh[1:(length(regh)-day_ahead)], variables]
-  if(day_ahead > 0) {y <- pun[tarh[day_ahead:length(tarh)],varn]; start_ix <- day_ahead}
+  if(day_ahead > 0) {y <- pun[tarh[(1+day_ahead):length(tarh)],varn]; start_ix <- day_ahead}
   else {y <- pun[tarh,varn]}
   
   dts <- dates(pun[,1])
-  hol <- add_holidays(dts)
+  hol <- add_holidays(asdts3)
   
   asdts <- dates2(dts)
-  day <- maply(1:length(asdts), function(n) convert_day_to_angle(convert_day(as.character(lubridate::wday(asdts[n],label=TRUE)))))
+  asdts2 <- unique(asdts)
+  asdts3 <- maply(1:length(asdts2), function(n) from_dates_to_char(asdts2[n]))
+  day <- maply(1:length(asdts2), function(n) convert_day_to_angle(convert_day(as.character(lubridate::wday(asdts2[n],label=TRUE)))))
   
-  tmin <- maply(1:(length(dts)-day_ahead), associate_meteo_ora(dts[n], meteo, "Tmin"))
-  tmax <- maply(1:(length(dts)-day_ahead), associate_meteo_ora(dts[n], meteo, "Tmax"))
-  tmed <- maply(1:(length(dts)-day_ahead), associate_meteo_ora(dts[n], meteo, "Tmedia"))
-  rain <- maply(1:(length(dts)-day_ahead), associate_meteo_ora(dts[n], meteo, "Pioggia"))
-  vm <- maply(1:(length(dts)-day_ahead), associate_meteo_ora(dts[n], meteo, "Vento_media"))
+  tmin <- tmax <- tmed <- rain <- vm <- ttmin <- ttmax <- ttmed <- train <- tvm <- c()
   
-  ttmin <- maply(1:length(y), associate_meteo_ora(dts[n+day_ahead], meteo, "Tmin"))
-  ttmax <- maply(1:length(y), associate_meteo_ora(dts[n+day_ahead], meteo, "Tmax"))
-  ttmed <- maply(1:length(y), associate_meteo_ora(dts[n+day_ahead], meteo, "Tmedia"))
-  train <- maply(1:length(y), associate_meteo_ora(dts[n+day_ahead], meteo, "Pioggia"))
-  tvm <- maply(1:length(y), associate_meteo_ora(dts[n+day_ahead], meteo, "Vento_media"))
+  for(n in 1:(length(asdts3)-day_ahead))
+  {
+    tmin <- c(tmin, associate_meteo_ora(asdts3[n], meteo, "Tmin"))
+    tmax <- c(tmax, associate_meteo_ora(asdts3[n], meteo, "Tmax"))
+    tmed <- c(tmed, associate_meteo_ora(asdts3[n], meteo, "Tmedia"))
+    rain <- c(rain, associate_meteo_ora(asdts3[n], meteo, "Pioggia"))
+    vm <- c(vm, associate_meteo_ora(asdts3[n], meteo, "Vento_media"))
+    
+    ttmin <- c(ttmin, associate_meteo_ora(asdts3[n+day_ahead], meteo, "Tmin"))
+    ttmax <- c(ttmax, associate_meteo_ora(asdts3[n+day_ahead], meteo, "Tmax"))
+    ttmed <- c(ttmed, associate_meteo_ora(asdts3[n+day_ahead], meteo, "Tmedia"))
+    train <- c(train, associate_meteo_ora(asdts3[n+day_ahead], meteo, "Pioggia"))
+    tvm <- c(tvm, associate_meteo_ora(asdts3[n+day_ahead], meteo, "Vento_media"))
+  }
   
   df <- data.frame(reg_val, day[1:(length(regh)-day_ahead)], hol[1:(length(regh)-day_ahead)], tmin, tmax, tmed, rain, vm, y,
-                   day[start_ix:(length(y)+day_ahead)], hol[start_ix:(length(y)+day_ahead)], ttmin, ttmax, ttmed, train, tvm, stringsAsFactors = FALSE)
+                   day[(1+day_ahead):(length(tarh))], hol[(1+day_ahead):(length(tarh))], ttmin, ttmax, ttmed, train, tvm, stringsAsFactors = FALSE)
   
-  colnames(df) <- Names
+  colnames(df) <- vanames
   return(df)
 }
 #####################################################################################################################################
@@ -74,7 +81,7 @@ from_dates_to_char <- function(dt)
 {
   return(paste0(unlist(strsplit(as.character(dt),"-"))[3],"/",unlist(strsplit(as.character(dt),"-"))[2],"/",unlist(strsplit(as.character(dt),"-"))[1]))
 }
-#####################################################################################################################################a
+#####################################################################################################################################
 get_meteo <- function(met)
 {
   cols <- which(tolower(colnames(met)) %in% c("tmin","tmax","tmedia","pioggia","ventomedia"))
@@ -90,4 +97,53 @@ get_meteo <- function(met)
   met2 <- data.frame(dts3,met[,cols])
   colnames(met2) <- c("Data", "Tmin","Tmax","Tmedia","Vento_media","Pioggia")
   return(met2)
+}
+##########################################################################################################################################
+generate_glm_datasets <- function(pun, meteo)
+{
+  gc()
+  pun <- data.frame(pun)
+  count <- 0
+  for(da in 0:2)
+  {
+    for(rh in 1:24)
+    {
+      th_a <- c(2:24,1)
+      th <- th_a[rh]
+      tryCatch(
+        {
+          start <- Sys.time()
+          #aug <- augmented_dataset(data1, data2, step = step , day_ahead = da)
+          trainset <- create_glm_dataset(pun, "PUN", rh, th, meteo, da)
+          trainset2 <- trainset[1:(nrow(trainset)-31),]
+          testset <- trainset[(nrow(trainset)-31):nrow(trainset),]
+          
+          trainseth2o <- as.h2o(trainset)
+          trainset2h2o <- as.h2o(trainset2)
+          testseth2o <- as.h2o(testset)
+          
+          name1 <- paste0("C:\\Users\\utente\\Documents\\PUN\\glm\\trainset_completo_rh_",rh,"_th_",th,"_dayahead_",da,".csv")
+          name2 <- paste0("C:\\Users\\utente\\Documents\\PUN\\glm\\trainset2_rh_",rh,"_th_",th,"_dayahead_",da,".csv")  
+          name3 <- paste0("C:\\Users\\utente\\Documents\\PUN\\glm\\testset_rh_",rh,"_th_",th,"_dayahead_",da,".csv")
+          
+          h2o.exportFile(trainseth2o, name1)
+          h2o.exportFile(trainset2h2o, name2)
+          h2o.exportFile(testseth2o, name3)
+          
+          rm(trainset); rm(trainseth2o); rm(testset); rm(testseth2o); rm(trainset2); rm(trainset2h2o);
+          print(paste("done regressor hour ",rh, " with target hour ", th," day ahead ", da, " and removed the files"))
+          
+          end <- Sys.time()
+          end-start
+          
+          count <- count + 1
+          print(paste0("passages left: ",24*3 - count))
+        }, error = function(cond)
+        {
+          message(cond)
+          print(paste("day ahead ", da, " rh ", rh, " and th ", th,  " failed"))
+        }
+      )
+    }
+  }
 }
