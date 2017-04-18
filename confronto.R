35168e843977473441f425e0902efa223142214a
davideflo <davideflo@users.noreply.github.com>
2017-04-18 17:36:35 +0200

    Hellinger Distance and comparison AP - K2E

--- confronto.R
+++ confronto.R
@@ -5,7 +5,16 @@ library(readxl)
 library(ggplot2)
 library(feather)
 library(lubridate)
+library(data.table)
+library(entropy)
+library(distrEx)
 
+################################################################################################
+filterDay <- function(df, weekday)
+{
+  return(df[which(lubridate::wday(as.Date(unlist(df["date"]), origin = '1899-12-30')) == weekday),])
+}
+################################################################################################
 
 ltp <- read_excel('longterm_pun.xlsx')
 fabb <- read_excel('Forecast annuale.xlsx')
@@ -57,8 +66,13 @@ abline(h = -1)
 
 ##############################################################################################################
 ### analysis of the distribusions of F1,F2, F3 given PK or OP
+### https://cran.r-project.org/web/packages/distrEx/distrEx.pdf
 
 real <- data.table(read_excel("DB_Borse_Elettriche_PER MI_17_conMacro - Copy.xlsm", sheet = 2))
+real <- real[which(!is.na(real[,13])), 1:13]
+colnames(real)[13] <- "PUN"
+colnames(real)[1] <- "date"
+
 
 dt <- data.table(read_excel("conddistr.xlsx"))
 
@@ -77,61 +91,146 @@ print(mean(dt$PUN[which(dt$`AEEG 181/06` == "F3" & (dt$`PEAK-OFF PEAK` == "PK" |
 print(mean(dt$PUN[which(dt$`AEEG 181/06` == "F3" & dt$`PEAK-OFF PEAK` == "OP")]))
 
 
-dt <- data.table(read_excel("longterm_pun.xlsx"))
+dtx <- data.table(read_excel("longterm_pun.xlsx"))
 
 par(mfrow = c(3,2))
-hist(dt$pun[which(dt$AEEG.181.06 == "F1" & (dt$PK.OP == "PK" | dt$PK.OP == "P"))], breaks = 20, col = 'red', main = "AP L(F1|PK)")
-hist(dt$pun[which(dt$AEEG.181.06 == "F2" & (dt$PK.OP == "PK" | dt$PK.OP == "P"))], breaks = 20, col = 'magenta', main = "AP L(F2|PK)")
-hist(dt$pun[which(dt$AEEG.181.06 == "F2" & dt$PK.OP == "OP")], breaks = 20, col = 'grey', main = "AP L(F2|OP)")
-hist(dt$pun[which(dt$AEEG.181.06 == "F3" & (dt$PK.OP == "PK" | dt$PK.OP == "P"))], breaks = 20, col = 'green', main = "AP L(F3|PK)")
-hist(dt$pun[which(dt$AEEG.181.06 == "F3" & dt$PK.OP == "OP")], breaks = 20, col = 'navy', main = "AP L(F3|OP)")
+hist(dtx$pun[which(dtx$AEEG.181.06 == "F1" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))], breaks = 20, col = 'red', main = "AP L(F1|PK)")
+hist(dtx$pun[which(dtx$AEEG.181.06 == "F2" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))], breaks = 20, col = 'magenta', main = "AP L(F2|PK)")
+hist(dtx$pun[which(dtx$AEEG.181.06 == "F2" & dtx$PK.OP == "OP")], breaks = 20, col = 'grey', main = "AP L(F2|OP)")
+hist(dtx$pun[which(dtx$AEEG.181.06 == "F3" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))], breaks = 20, col = 'green', main = "AP L(F3|PK)")
+hist(dtx$pun[which(dtx$AEEG.181.06 == "F3" & dtx$PK.OP == "OP")], breaks = 20, col = 'navy', main = "AP L(F3|OP)")
 
-quantile(dt$pun[which(dt$AEEG.181.06 == "F1" & (dt$PK.OP == "PK" | dt$PK.OP == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
-quantile(dt$pun[which(dt$AEEG.181.06 == "F2" & (dt$PK.OP == "PK" | dt$PK.OP == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
-quantile(dt$pun[which(dt$AEEG.181.06 == "F2" & dt$PK.OP == "OP")], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
-quantile(dt$pun[which(dt$AEEG.181.06 == "F3" & (dt$PK.OP == "PK" | dt$PK.OP == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
-quantile(dt$pun[which(dt$AEEG.181.06 == "F3" & dt$PK.OP == "OP")], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtx$pun[which(dtx$AEEG.181.06 == "F1" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtx$pun[which(dtx$AEEG.181.06 == "F2" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtx$pun[which(dtx$AEEG.181.06 == "F2" & dtx$PK.OP == "OP")], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtx$pun[which(dtx$AEEG.181.06 == "F3" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtx$pun[which(dtx$AEEG.181.06 == "F3" & dtx$PK.OP == "OP")], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
 
-sd(dt$pun[which(dt$AEEG.181.06 == "F1" & (dt$PK.OP == "PK" | dt$PK.OP == "P"))])
-sd(dt$pun[which(dt$AEEG.181.06 == "F2" & (dt$PK.OP == "PK" | dt$PK.OP == "P"))])
-sd(dt$pun[which(dt$AEEG.181.06 == "F2" & dt$PK.OP == "OP")])
-sd(dt$pun[which(dt$AEEG.181.06 == "F3" & (dt$PK.OP == "PK" | dt$PK.OP == "P"))])
-sd(dt$pun[which(dt$AEEG.181.06 == "F3" & dt$PK.OP == "OP")])
+sd(dtx$pun[which(dtx$AEEG.181.06 == "F1" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))])
+sd(dtx$pun[which(dtx$AEEG.181.06 == "F2" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))])
+sd(dtx$pun[which(dtx$AEEG.181.06 == "F2" & dtx$PK.OP == "OP")])
+sd(dtx$pun[which(dtx$AEEG.181.06 == "F3" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))])
+sd(dtx$pun[which(dtx$AEEG.181.06 == "F3" & dtx$PK.OP == "OP")])
 
 par(mfrow = c(3,1))
-hist(dt$pun[which(dt$AEEG.181.06 == "F1")], breaks = 20, col = 'red', main = "AP L(F1)")
-hist(dt$pun[which(dt$AEEG.181.06 == "F2")], breaks = 20, col = 'magenta', main = "AP L(F2)")
-hist(dt$pun[which(dt$AEEG.181.06 == "F3")], breaks = 20, col = 'green', main = "AP L(F3)")
+hist(dtx$pun[which(dtx$AEEG.181.06 == "F1")], breaks = 20, col = 'red', main = "AP L(F1)")
+hist(dtx$pun[which(dtx$AEEG.181.06 == "F2")], breaks = 20, col = 'magenta', main = "AP L(F2)")
+hist(dtx$pun[which(dtx$AEEG.181.06 == "F3")], breaks = 20, col = 'green', main = "AP L(F3)")
 
 
-dt <- data.table(read_excel("Copy of FWD2017-2018.xlsx"))
+dtk <- data.table(read_excel("Copy of FWD2017-2018.xlsx"))
+colnames(dtk)[1] <- "date"
 
 par(mfrow = c(3,2))
-hist(dt$ITALIA[which(dt$Fasce == "F1" & (dt$`PK/OP` == "PK" | dt$`PK/OP` == "P"))], breaks = 20, col = 'red', main = "K2E L(F1|PK)")
-hist(dt$ITALIA[which(dt$Fasce == "F2" & (dt$`PK/OP` == "PK" | dt$`PK/OP` == "P"))], breaks = 20, col = 'magenta', main = "K2E L(F2|PK)")
-hist(dt$ITALIA[which(dt$Fasce == "F2" & dt$`PK/OP` == "OP")], breaks = 20, col = 'grey', main = "K2E L(F2|OP)")
-hist(dt$ITALIA[which(dt$Fasce == "F3" & (dt$`PK/OP` == "PK" | dt$`PK/OP` == "P"))], breaks = 20, col = 'green', main = "K2E L(F3|PK)")
-hist(dt$ITALIA[which(dt$Fasce == "F3" & dt$`PK/OP` == "OP")], breaks = 20, col = 'navy', main = "K2E L(F3|OP)")
+hist(dtk$ITALIA[which(dtk$Fasce == "F1" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))], breaks = 20, col = 'red', main = "K2E L(F1|PK)")
+hist(dtk$ITALIA[which(dtk$Fasce == "F2" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))], breaks = 20, col = 'magenta', main = "K2E L(F2|PK)")
+hist(dtk$ITALIA[which(dtk$Fasce == "F2" & dtk$`PK/OP` == "OP")], breaks = 20, col = 'grey', main = "K2E L(F2|OP)")
+hist(dtk$ITALIA[which(dtk$Fasce == "F3" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))], breaks = 20, col = 'green', main = "K2E L(F3|PK)")
+hist(dtk$ITALIA[which(dtk$Fasce == "F3" & dtk$`PK/OP` == "OP")], breaks = 20, col = 'navy', main = "K2E L(F3|OP)")
 
-quantile(dt$ITALIA[which(dt$Fasce == "F1" & (dt$`PK/OP` == "PK" | dt$`PK/OP` == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
-quantile(dt$ITALIA[which(dt$Fasce == "F2" & (dt$`PK/OP` == "PK" | dt$`PK/OP` == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
-quantile(dt$ITALIA[which(dt$Fasce == "F2" & dt$`PK/OP` == "OP")], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
-quantile(dt$ITALIA[which(dt$Fasce == "F3" & (dt$`PK/OP` == "PK" | dt$`PK/OP` == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
-quantile(dt$ITALIA[which(dt$Fasce == "F3" & dt$`PK/OP` == "OP")], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtk$ITALIA[which(dtk$Fasce == "F1" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtk$ITALIA[which(dtk$Fasce == "F2" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtk$ITALIA[which(dtk$Fasce == "F2" & dtk$`PK/OP` == "OP")], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtk$ITALIA[which(dtk$Fasce == "F3" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
+quantile(dtk$ITALIA[which(dtk$Fasce == "F3" & dtk$`PK/OP` == "OP")], probs = c(0.5,0.6,0.7,0.8,0.9,0.95))
 
-sd(dt$ITALIA[which(dt$Fasce == "F1" & (dt$`PK/OP` == "PK" | dt$`PK/OP` == "P"))])
-sd(dt$ITALIA[which(dt$Fasce == "F2" & (dt$`PK/OP` == "PK" | dt$`PK/OP` == "P"))])
-sd(dt$ITALIA[which(dt$Fasce == "F2" & dt$`PK/OP` == "OP")])
-sd(dt$ITALIA[which(dt$Fasce == "F3" & (dt$`PK/OP` == "PK" | dt$`PK/OP` == "P"))])
-sd(dt$ITALIA[which(dt$Fasce == "F3" & dt$`PK/OP` == "OP")])
+sd(dtk$ITALIA[which(dtk$Fasce == "F1" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))])
+sd(dtk$ITALIA[which(dtk$Fasce == "F2" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))])
+sd(dtk$ITALIA[which(dtk$Fasce == "F2" & dtk$`PK/OP` == "OP")])
+sd(dtk$ITALIA[which(dtk$Fasce == "F3" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))])
+sd(dtk$ITALIA[which(dtk$Fasce == "F3" & dtk$`PK/OP` == "OP")])
 
 par(mfrow = c(3,1))
-hist(dt$ITALIA[which(dt$Fasce == "F1")], breaks = 20, col = 'red', main = "K2E L(F1)")
-hist(dt$ITALIA[which(dt$Fasce == "F2")], breaks = 20, col = 'magenta', main = "K2E L(F2)")
-hist(dt$ITALIA[which(dt$Fasce == "F3")], breaks = 20, col = 'green', main = "K2E L(F3)")
+hist(dtk$ITALIA[which(dtk$Fasce == "F1")], breaks = 20, col = 'red', main = "K2E L(F1)")
+hist(dtk$ITALIA[which(dtk$Fasce == "F2")], breaks = 20, col = 'magenta', main = "K2E L(F2)")
+hist(dtk$ITALIA[which(dtk$Fasce == "F3")], breaks = 20, col = 'green', main = "K2E L(F3)")
+
+###### "distance" (KL-Divergence) between the distribution -- Hellinger Distance ####
+HellingerDistance <- function(dist1, dist2)
+{
+ d1 <- density(dist1)
+ d2 <- density(dist2)
+ m <- min(d1$x, d2$x)
+ M <- max(d1$x, d2$x)
+ d1 <- density(dist1, from = m, to = M, n = 1024)
+ d2 <- density(dist2, from = m, to = M, n = 1024)
+ 
+ dS <- (sqrt(d1$y) - sqrt(d2$y))^2
+ 
+ I <- 0
+ for(i in 1:(length(d1$x)-1))
+ {
+  I <- I + (dS[i+1] + dS[i])*(d1$x[i+1] - d1$x[i])/2  
+ }
+ 
+ return((1/2)*I)
+ 
+}
+###################################################################################
+### I.T.O. PK/OP:
+
+### AP vs real2017:
+HellingerDistance(real$PUN[which(real$`PEAK-OFF PEAK` == "PK" | real$`PEAK-OFF PEAK` == "P")], dtx$pun[which(dtx$PK.OP == "PK" | dtx$PK.OP == "P")])
+HellingerDistance(real$PUN[which(real$`PEAK-OFF PEAK` == "OP")], dtx$pun[which(dtx$PK.OP == "OP")])
+
+### K2E vs real2017:
+HellingerDistance(real$PUN[which(real$`PEAK-OFF PEAK` == "PK" | real$`PEAK-OFF PEAK` == "P")], dtk$ITALIA[which(dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P")])
+HellingerDistance(real$PUN[which(real$`PEAK-OFF PEAK` == "OP")], dtk$ITALIA[which(dtk$`PK/OP` == "OP")])
+
+### I.T.O. F1, F2, F3:
+
+### AP vs real2017:
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F1")], dtx$pun[which(dtx$AEEG.181.06 == "F1")])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F2")], dtx$pun[which(dtx$AEEG.181.06 == "F2")])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F3")], dtx$pun[which(dtx$AEEG.181.06 == "F3")])
+
+### K2E vs real2017:
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F1")], dtk$ITALIA[which(dtk$Fasce == "F1")])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F2")], dtk$ITALIA[which(dtk$Fasce == "F2")])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F3")], dtk$ITALIA[which(dtk$Fasce == "F3")])
+
+### I.T.O. PK/OP divided in F1, F2, F3:
+
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F1" & (real$`PEAK-OFF PEAK` == "PK" | real$`PEAK-OFF PEAK` == "P"))],
+                                 dtx$pun[which(dtx$AEEG.181.06 == "F1" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F2" & (real$`PEAK-OFF PEAK` == "PK" | real$`PEAK-OFF PEAK` == "P"))],
+                  dtx$pun[which(dtx$AEEG.181.06 == "F2" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F2" & real$`PEAK-OFF PEAK` == "OP")],
+                  dtx$pun[which(dtx$AEEG.181.06 == "F2" & dtx$PK.OP == "OP")])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F3" & (real$`PEAK-OFF PEAK` == "PK" | real$`PEAK-OFF PEAK` == "P"))],
+                  dtx$pun[which(dtx$AEEG.181.06 == "F3" & (dtx$PK.OP == "PK" | dtx$PK.OP == "P"))])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F3" & real$`PEAK-OFF PEAK` == "OP")],
+                  dtx$pun[which(dtx$AEEG.181.06 == "F3" & dtx$PK.OP == "OP")])
+
+
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F1" & (real$`PEAK-OFF PEAK` == "PK" | real$`PEAK-OFF PEAK` == "P"))],
+                  dtk$ITALIA[which(dtk$Fasce == "F1" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F2" & (real$`PEAK-OFF PEAK` == "PK" | real$`PEAK-OFF PEAK` == "P"))],
+                  dtk$ITALIA[which(dtk$Fasce == "F2" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F2" & real$`PEAK-OFF PEAK` == "OP")],
+                  dtk$ITALIA[which(dtk$Fasce == "F2" & dtk$`PK/OP` == "OP")])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F3" & (real$`PEAK-OFF PEAK` == "PK" | real$`PEAK-OFF PEAK` == "P"))],
+                  dtk$ITALIA[which(dtk$Fasce == "F3" & (dtk$`PK/OP` == "PK" | dtk$`PK/OP` == "P"))])
+HellingerDistance(real$PUN[which(real$`AEEG 181/06` == "F3" & real$`PEAK-OFF PEAK` == "OP")],
+                  dtk$ITALIA[which(dtk$Fasce == "F3" & dtk$`PK/OP` == "OP")])
 
 
 
+### day-by-day: 1 = "Sunday", ..., 7 = "Saturday"
 
+HellingerDistance(filterDay(data.frame(real), 1)$PUN, filterDay(data.frame(dtx),1)$pun)
+HellingerDistance(filterDay(data.frame(real), 2)$PUN, filterDay(data.frame(dtx),2)$pun)
+HellingerDistance(filterDay(data.frame(real), 3)$PUN, filterDay(data.frame(dtx),3)$pun)
+HellingerDistance(filterDay(data.frame(real), 4)$PUN, filterDay(data.frame(dtx),4)$pun)
+HellingerDistance(filterDay(data.frame(real), 5)$PUN, filterDay(data.frame(dtx),5)$pun)
+HellingerDistance(filterDay(data.frame(real), 6)$PUN, filterDay(data.frame(dtx),6)$pun)
+HellingerDistance(filterDay(data.frame(real), 7)$PUN, filterDay(data.frame(dtx),7)$pun)
 
+HellingerDistance(filterDay(data.frame(real), 1)$PUN, filterDay(data.frame(dtk),1)$ITALIA)
+HellingerDistance(filterDay(data.frame(real), 2)$PUN, filterDay(data.frame(dtk),2)$ITALIA)
+HellingerDistance(filterDay(data.frame(real), 3)$PUN, filterDay(data.frame(dtk),3)$ITALIA)
+HellingerDistance(filterDay(data.frame(real), 4)$PUN, filterDay(data.frame(dtk),4)$ITALIA)
+HellingerDistance(filterDay(data.frame(real), 5)$PUN, filterDay(data.frame(dtk),5)$ITALIA)
+HellingerDistance(filterDay(data.frame(real), 6)$PUN, filterDay(data.frame(dtk),6)$ITALIA)
+HellingerDistance(filterDay(data.frame(real), 7)$PUN, filterDay(data.frame(dtk),7)$ITALIA)
 
