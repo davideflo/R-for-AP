c3f00b27ecb8105dfa7b2d0a453d2da4ddbddd2e
davideflo <davideflo@users.noreply.github.com>
2017-04-26 16:52:15 +0200

    Vectorized ExpectedPayOff

--- simulation_CL.R
+++ simulation_CL.R
@@ -2,19 +2,31 @@
 library(Rsolnp)
 
 
-ExpectedPayOff <- function(p = 0, q = 0, r = 0, QJ = 0, QR = 0, QA = 0)
+ExpectedPayOff <- function(Q)
 {
+  QJ <- Q[1]
+  QR <- Q[2]
+  QA <- Q[3]
   vJ <- 2.60
   vR <- 2.70
   vA <- 5
   vM <- 8.50
   
   EF <- 0
-  
-  EF <- p*vJ*QJ - (1 - p)*(QR*vR + QA*vA + (100 - QJ - QR - QA)*vM) 
-  EF <- EF + q*vA*QA - (1 - q)*(QJ*vJ + QR*vR + (100 - QJ - QR - QA)*vM) 
-  EF <- EF + r*vR*QR - (1 - r)*(QJ*vJ + QA*vA + (100 - QJ - QR - QA)*vM) 
-  EF <- EF + (1 - p - q - r)*(100 - QJ - QR - QA)*vM - (p + q + r)*(QJ*vJ + QA*vA + QR*vR)
+  x <- seq(0, 1, 0.01)
+  for(p in x)
+  {
+    for(q in x[which(x == p):100])
+    {
+      for(r in x[which(x == q):100])
+      {
+        EF <- p*vJ*QJ - (1 - p)*(QR*vR + QA*vA + (100 - QJ - QR - QA)*vM) 
+        EF <- EF + q*vA*QA - (1 - q)*(QJ*vJ + QR*vR + (100 - QJ - QR - QA)*vM) 
+        EF <- EF + r*vR*QR - (1 - r)*(QJ*vJ + QA*vA + (100 - QJ - QR - QA)*vM) 
+        EF <- EF + (1 - p - q - r)*(100 - QJ - QR - QA)*vM - (p + q + r)*(QJ*vJ + QA*vA + QR*vR)
+      }
+    }
+  }
   
   return((-1)*EF)
 }
@@ -24,26 +36,41 @@ ExpectedPayOff <- function(p = 0, q = 0, r = 0, QJ = 0, QR = 0, QA = 0)
 ### feasible region: ui %*% theta - ci >= 0
 
 
-ui <- matrix(c(-1,-1,-1,0,0,0,
-               0,0,0,-1,-1,-1,
-               -1,0,0,0,0,0,
-               0,-1,0,0,0,0,
-               0,0,-1,0,0,0,
-               0,0,0,-1,0,0,
-               0,0,0,0,-1,0,
-               0,0,0,0,0,-1,
-               1,0,0,0,0,0,
-               0,1,0,0,0,0,
-               0,0,1,0,0,0,
-               0,0,0,1,0,0,
-               0,0,0,0,1,0,
-               0,0,0,0,0,1), ncol = 6, byrow = TRUE)
+# ui <- matrix(c(-1,-1,-1,0,0,0,
+#                0,0,0,-1,-1,-1,
+#                -1,0,0,0,0,0,
+#                0,-1,0,0,0,0,
+#                0,0,-1,0,0,0,
+#                0,0,0,-1,0,0,
+#                0,0,0,0,-1,0,
+#                0,0,0,0,0,-1,
+#                1,0,0,0,0,0,
+#                0,1,0,0,0,0,
+#                0,0,1,0,0,0,
+#                0,0,0,1,0,0,
+#                0,0,0,0,1,0,
+#                0,0,0,0,0,1), ncol = 6, byrow = TRUE)
+# 
+# theta <- c(0.20,0.20,0.20,20,20,20)
+# ci <- c(1,100,1,1,1,100,100,100,0,0,0,0,0,0)
+# 
+# ui %*% theta - ci
 
-theta <- c(0.20,0.20,0.20,20,20,20)
-ci <- c(1,100,1,1,1,100,100,100,0,0,0,0,0,0)
+ui <- matrix(c(-1,-1,-1,
+               -1,0,0,
+               0,-1,0,
+               0,0,-1,
+               1,0,0,
+               0,1,0,
+               0,0,1), ncol = 3, byrow = TRUE)
+
+theta <- c(20,20,20)
+ci <- c(-100,-100,-100,-100,0,0,0)
 
 ui %*% theta - ci
 
-constrOptim(c(0.20, 0.20, 0.20, 10, 10, 10), ExpectedPayOff, method = "Nelder-Mead", ui = ui, ci = c(-1,-100,-1,-1,-1,-100,-100,-100,0,0,0,0,0,0))
+constrOptim(c(10, 10, 10), ExpectedPayOff, method = "Nelder-Mead", ui = ui, ci = ci)
+#constrOptim(c(10, 10, 10), ExpectedPayOff, method = "L-BFGS-B", ui = ui, ci = ci)
 
-ExpectedPayOff(theta)
+
+ExpectedPayOff(theta/3)
