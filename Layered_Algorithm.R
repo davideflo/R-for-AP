0a8dd7580c00f8e3e14dfd451d76b2c7a330cb2b
davideflo <davideflo@users.noreply.github.com>
2017-07-26 18:02:48 +0200

    Layered Algorithm

--- Layered_Algorithm.R
+++ Layered_Algorithm.R
@@ -1,4 +1,5 @@
-####### TRADING OPTIMIZATION
+############### 
+
 
 ###########################################################################################
 get_Signals2 <- function(dt, Ta, Sl, tau = 0.05, nm = 5, ns = 5, tau2 = 1.4, bVerbose = FALSE)
@@ -69,12 +70,12 @@ get_Signals2 <- function(dt, Ta, Sl, tau = 0.05, nm = 5, ns = 5, tau2 = 1.4, bVe
     }
   }
   if(bVerbose){
-  plot(dt$Last, type = "l", col = "red", lwd = 1.5)
-  lines(rm, type = "l", col = "royalblue4", lwd = 1.5)
-  lines(up, type = "l", col = "orangered", lwd = 1.5)
-  lines(low, type = "l", col = "orangered", lwd = 1.5)
-  points(df$dove[which(df$posizione_vendita == 1)], dt$Last[df$dove[which(df$posizione_vendita == 1)]], pch = 16, col = "gold")
-  points(df$dove[which(df$posizione_acquisto == 1)], dt$Last[df$dove[which(df$posizione_acquisto == 1)]], pch = 16, col = "purple")
+    plot(dt$Last, type = "l", col = "red", lwd = 1.5)
+    lines(rm, type = "l", col = "royalblue4", lwd = 1.5)
+    lines(up, type = "l", col = "orangered", lwd = 1.5)
+    lines(low, type = "l", col = "orangered", lwd = 1.5)
+    points(df$dove[which(df$posizione_vendita == 1)], dt$Last[df$dove[which(df$posizione_vendita == 1)]], pch = 16, col = "gold")
+    points(df$dove[which(df$posizione_acquisto == 1)], dt$Last[df$dove[which(df$posizione_acquisto == 1)]], pch = 16, col = "purple")
   }
   return(df)
 }
@@ -106,17 +107,19 @@ get_SignalsLayers <- function(dt, dts, layer, Ta, Sl, tau = 0.05, nm = 5, ns = 5
     sell <- dts$posizione_vendita[j]
     val <- dt$Last[da]
     Target <- dts$target[j]
+    StopLoss <- dts$StopLoss[j]
     
     if(da == nrow(dt)) break
     
     for(i in 1:nrow(dtda))
     {
+      prices <- orderPrices(c(dtda$Open[i],dtda$High[i],dtda$Low[i],dtda$Last[i]))
       if(layered < layer & buy == 1)
       {
-        if(dtda$Last[i] <= (val - threshold))
+        if(any(prices <= (val - threshold))) 
         {
-          d.f <- data.frame(data = dtda$`Date GMT`[i], posizione_vendita = 0, posizione_acquisto = 1, first = dtda$Last[i], dove = (da + i), 
-                            target = dtda$Last[i] + Ta, StopLoss = dtda$Last[i] - Sl, Layer = layered + 1)
+          d.f <- data.frame(data = dtda$`Date GMT`[i], posizione_vendita = 0, posizione_acquisto = 1, first = (val - threshold), dove = (da + i), 
+                            target = Target, StopLoss = StopLoss, Layer = layered + 1)
           l <- list(df, d.f)
           df <- rbindlist(l)
           break
@@ -132,10 +135,10 @@ get_SignalsLayers <- function(dt, dts, layer, Ta, Sl, tau = 0.05, nm = 5, ns = 5
       }
       else if(layered < layer & sell == 1)
       {
-        if(dtda$Last[i] >= (val + threshold))
+        if(any(prices >= (val + threshold)))
         { 
-          d.f <- data.frame(data = dtda$`Date GMT`[i], posizione_vendita = 1, posizione_acquisto = 0, first = dtda$Last[i], dove = (da + i), 
-                            target = dtda$Last[i] - Ta, StopLoss = dtda$Last[i] + Sl, Layer = layered + 1)
+          d.f <- data.frame(data = dtda$`Date GMT`[i], posizione_vendita = 1, posizione_acquisto = 0, first = (val + threshold), dove = (da + i), 
+                            target = Target, StopLoss = StopLoss, Layer = layered + 1)
           l <- list(df, d.f)
           df <- rbindlist(l)  
           break
@@ -179,144 +182,6 @@ orderPrices <- function(x)
   }
 }
 ###############################################################################################
-get_Closures2 <- function(dt, dts)
-{
-  ldf <- data_frame()
-  for(i in 1:nrow(dts))
-  {
-    start <- dts$dove[i]
-    target <- dts$target[i]
-    SL <- dts$StopLoss[i]
-    dt2 <- dt[(start+1):nrow(dt),]
-    vs <- dt$Last[start]
-    ven <- dts$posizione_vendita[i]
-    acq <- dts$posizione_acquisto[i]
-    giorno <- dts$data[i]
-    ora <- ifelse(!is.null(dt$Time[start]),dt$Time[start],0)
-    if(start == nrow(dt))
-    {
-      break
-    }
-    for(j in 1:nrow(dt2))
-    {
-      closure <- 0
-      pl <- 0
-      data_chiusura <- 0
-      prices <- orderPrices(c(dt2$Open[j],dt2$High[j],dt2$Low[j],dt2$Last[j]))
-      sel <- 0
-      if(ven == 1)
-      {
-        if(any(prices <= target) & !any(prices >= SL))
-        {
-          data_chiusura <- dt2$`Date GMT`[j]
-          closure <- target
-          pl <- (-closure + vs - 0.054)*8760
-          d.f <- data.frame(data = giorno, ora = ora, val_inizio = vs, posizione_vendita = ven, posizione_acquisto = acq, target = target, stoploss = SL, chiusura = closure, 
-                            d_chiusura = data_chiusura, P_L = pl)
-          l <- list(ldf, d.f)
-          ldf <- rbindlist(l)
-          break
-        }
-        else if(!any(prices <= target) & any(prices >= SL))
-        {
-          data_chiusura <- dt2$`Date GMT`[j]
-          closure <- SL
-          pl <- (-closure + vs - 0.054)*8760
-          d.f <- data.frame(data = giorno, ora = ora, val_inizio = vs, posizione_vendita = ven, posizione_acquisto = acq, target = target, stoploss = SL, chiusura = closure, 
-                            d_chiusura = data_chiusura, P_L = pl)
-          l <- list(ldf, d.f)
-          ldf <- rbindlist(l)
-          break
-        }
-        else if(any(prices <= target) & any(prices >= SL))
-        {
-          if(min(which(prices <= target)) < min(prices >= SL))
-          {
-            data_chiusura <- dt2$`Date GMT`[j]
-            closure <- target
-            pl <- (-closure + vs - 0.054)*8760
-            d.f <- data.frame(data = giorno, ora = ora, val_inizio = vs, posizione_vendita = ven, posizione_acquisto = acq, target = target, stoploss = SL, chiusura = closure, 
-                              d_chiusura = data_chiusura, P_L = pl)
-            l <- list(ldf, d.f)
-            ldf <- rbindlist(l)
-            break
-          }
-          else
-          {
-            data_chiusura <- dt2$`Date GMT`[j]
-            closure <- SL
-            pl <- (closure - vs - 0.054)*8760
-            d.f <- data.frame(data = giorno, ora = ora, val_inizio = vs, posizione_vendita = ven, posizione_acquisto = acq, target = target, stoploss = SL, chiusura = closure, 
-                              d_chiusura = data_chiusura, P_L = pl)
-            l <- list(ldf, d.f)
-            ldf <- rbindlist(l)
-            break
-          }
-        }
-        else
-        {
-          next
-        }
-      }
-      else if(acq == 1)
-      {
-        if(any(prices >= target) & !any(prices <= SL))
-        {
-          data_chiusura <- dt2$`Date GMT`[j]
-          closure <- target
-          pl <- (closure - vs - 0.054)*8760
-          d.f <- data.frame(data = giorno, ora = ora, val_inizio = vs, posizione_vendita = ven, posizione_acquisto = acq, target = target, stoploss = SL, chiusura = closure, 
-                            d_chiusura = data_chiusura, P_L = pl)
-          l <- list(ldf, d.f)
-          ldf <- rbindlist(l)
-          break
-        }
-        else if(!any(prices >= target) & any(prices <= SL))
-        {
-          data_chiusura <- dt2$`Date GMT`[j]
-          closure <- SL
-          pl <- (closure - vs - 0.054)*8760
-          d.f <- data.frame(data = giorno, ora = ora, val_inizio = vs, posizione_vendita = ven, posizione_acquisto = acq, target = target, stoploss = SL, chiusura = closure, 
-                            d_chiusura = data_chiusura, P_L = pl)
-          l <- list(ldf, d.f)
-          ldf <- rbindlist(l)
-          break
-        }
-        else if(any(prices >= target) & any(prices <= SL))
-        {
-          if(min(which(prices <= target)) < min(prices >= SL))
-          {
-            data_chiusura <- dt2$`Date GMT`[j]
-            closure <- target
-            pl <- (closure - vs - 0.054)*8760
-            d.f <- data.frame(data = giorno, ora = ora, val_inizio = vs, posizione_vendita = ven, posizione_acquisto = acq, target = target, stoploss = SL, chiusura = closure, 
-                              d_chiusura = data_chiusura, P_L = pl)
-            l <- list(ldf, d.f)
-            ldf <- rbindlist(l)
-            break
-          }
-          else
-          {
-            data_chiusura <- dt2$`Date GMT`[j]
-            closure <- SL
-            pl <- (closure - vs - 0.054)*8760
-            d.f <- data.frame(data = giorno, ora = ora, val_inizio = vs, posizione_vendita = ven, posizione_acquisto = acq, target = target, stoploss = SL, chiusura = closure, 
-                              d_chiusura = data_chiusura, P_L = pl)
-            l <- list(ldf, d.f)
-            ldf <- rbindlist(l)
-            break
-          }
-        }
-        else
-        {
-          next
-        }
-      }
-    }
-  }
-  #return(ldf[which(ldf$data <= ldf$d_chiusura),])
-  return(ldf)
-}
 ###############################################################################################
 get_ClosuresLayers <- function(dt, dts)
 {
@@ -327,12 +192,20 @@ get_ClosuresLayers <- function(dt, dts)
     target <- dts$target[i]
     SL <- dts$StopLoss[i]
     dt2 <- dt[(start+1):nrow(dt),]
+    
+    lay <- dts$Layer[i]
+    
     vs <- dt$Last[start]
+    
+    if(lay > 0)
+    {
+      vs <- dts$first[i]
+    }
+    
     ven <- dts$posizione_vendita[i]
     acq <- dts$posizione_acquisto[i]
     giorno <- dts$data[i]
     
-    lay <- dts$Layer[i]
     
     ora <- ifelse(!is.null(dt$Time[start]),dt$Time[start],0)
     if(start == nrow(dt))
@@ -457,17 +330,6 @@ get_ClosuresLayers <- function(dt, dts)
     }
   }
   return(ldf)
-}
-#################################################################################################################################
-GetOptimVals <- function(X)
-{
-  ger <- data.table(read_excel("H:/Energy Management/13. TRADING/Dati_Bollinger_GER.xlsx", sheet = "DATI NEW"))
-  #ger <- data.table(read_excel("H:/Energy Management/13. TRADING/GER_giornaliero.xlsx"))
-  #ger <- data.table(read_excel("H:/Energy Management/13. TRADING/GER_17_CAND.xlsx"))
-  #ger <- data.table(read_excel("H:/Energy Management/13. TRADING/GER_1718.xlsx"))
-  ddf <- get_Signals2(ger, X[1], X[2])
-  ldf <- get_Closures2(ger, ddf)
-  return(-sum(ldf$P_L))
 }
 #################################################################################################################################
 GetOptimValsLayered <- function(X)
@@ -496,4 +358,4 @@ GetOptimValsLayered <- function(X)
   
   return(-sum(ldf$P_L * weights))
 }
-write.xlsx(ldf, 'ger_bollinger_ora_5gg_1.4_strat_1_1.1.xlsx', row.names = FALSE)
+write.xlsx(ldf, 'ger_bollinger_ora_5gg_1.4_strat_1_1.1_4.xlsx', row.names = FALSE)
