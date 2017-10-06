56a479992459ac257e898133f5d6f890d6938bca
davideflo <davideflo@users.noreply.github.com>
2017-10-06 17:42:07 +0200

    Variance for random forest estimator

--- RF_forecast.R
+++ RF_forecast.R
@@ -9,6 +9,11 @@ library(data.table)
 library(randomForest)
 library(RFinfer)
 
+# references:
+# https://cran.r-project.org/web/packages/RFinfer/vignettes/IJ_vig.html
+# https://cran.r-project.org/web/packages/randomForest/randomForest.pdf
+# https://cran.rstudio.com/web/packages/RFinfer/RFinfer.pdf
+
 DWT <- data.table(read_excel('C:/Users/utente/Documents/DWT.xlsx'))
 
 rf <- randomForest(y ~ ., data = DWT, ntree = 3000,  keep.inbag = TRUE)
@@ -20,5 +25,6 @@ plot(1:243, DWT$y, pch = 16, col = 'blue')
 points(1:243, rfpv$pred, pch = 17, col = 'red')
 
 plot(rfpv$pred, type = 'l', col = 'black', lwd = 2)
+lines(DWT$y, type = 'l', col = 'purple')
 lines(rfpv$l.ci, type = 'l', col = 'red')
 lines(rfpv$u.ci, type = 'l', col = 'black')
